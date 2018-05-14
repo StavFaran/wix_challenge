15cc502beb58697012b85c38652564b3eb7abc97
StavFaran <33900682+StavFaran@users.noreply.github.com>
2018-05-15 00:00:51 +0300

    Add files via upload

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -6,6 +6,7 @@ import './Image.scss';
 class Image extends React.Component {
   static propTypes = {
     dto: PropTypes.object,
+    deleteImg: PropTypes.func,
     galleryWidth: PropTypes.number
   };
 
@@ -13,7 +14,8 @@ class Image extends React.Component {
     super(props);
     this.calcImageSize = this.calcImageSize.bind(this);
     this.state = {
-      size: 200
+      size: 200,
+      rotation: 0
     };
   }
 
@@ -34,6 +36,12 @@ class Image extends React.Component {
   urlFromDto(dto) {
     return `https://farm${dto.farm}.staticflickr.com/${dto.server}/${dto.id}_${dto.secret}.jpg`;
   }
+  rotateImg(){
+    var newRotation = (this.state.rotation + 90) % 360;
+    this.setState({
+      rotation: newRotation
+    });
+  }
 
   render() {
     return (
@@ -42,12 +50,13 @@ class Image extends React.Component {
         style={{
           backgroundImage: `url(${this.urlFromDto(this.props.dto)})`,
           width: this.state.size + 'px',
-          height: this.state.size + 'px'
-        }}
+          height: this.state.size + 'px',
+          transform: 'rotate('+this.state.rotation+'deg)'}
+        }
         >
-        <div>
-          <FontAwesome className="image-icon" name="sync-alt" title="rotate"/>
-          <FontAwesome className="image-icon" name="trash-alt" title="delete"/>
+        <div style={{transform: 'rotate('+(-this.state.rotation)+'deg)'}}>
+          <FontAwesome className="image-icon" onClick={()=>this.rotateImg() } name="sync-alt" title="rotate"/>
+          <FontAwesome className="image-icon" onClick={()=>this.props.deleteImg(this.props.dto) } name="trash-alt" title="delete"/>
           <FontAwesome className="image-icon" name="expand" title="expand"/>
         </div>
       </div>
